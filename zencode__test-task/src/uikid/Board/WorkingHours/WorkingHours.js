82d2ce77e3b3ea253eb32e75824160019cccaf3f
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-03-03 17:54:35 +0300

    Update WorkingHours.js

--- zencode__test-task/src/uikid/Board/WorkingHours/WorkingHours.js
+++ zencode__test-task/src/uikid/Board/WorkingHours/WorkingHours.js
@@ -1,55 +1,12 @@
 import React from 'react'
 import clases from './WorkingHours.module.css'
 import MounthsWeeks from './MounthsWeeks/MounthsWeeks'
+import RowsGroup from './RowsGroup/RowsGroup'
 let WorkingHours = () => {
   return (
     <tfoot className={clases.workingHours}>
       <MounthsWeeks />
-      <tr>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-      </tr>
-      <tr>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-      </tr>
-      <tr>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-      </tr>
+      <RowsGroup />
     </tfoot>
   )
 }
