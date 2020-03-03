484c67d6f33d60eede1d33dcbe42b896bca73de9
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-03-03 17:29:43 +0300

    Update ColumnsGroup.js

--- zencode__test-task/src/uikid/Board/ColumnsGroup/ColumnsGroup.js
+++ zencode__test-task/src/uikid/Board/ColumnsGroup/ColumnsGroup.js
@@ -1,27 +1,13 @@
 import React from 'react'
 import clases from './ColumnsGroup.module.css'
+import Column from './Column/Column'
 
 let ColumnsGroup = () => {
   return (
     <tbody className={clases.columnsGroup}>
-      <tr className={clases.column}>
-        <th className={clases.title}>Фамилия Имя</th>
-        <td className={clases.cell}>Имя 1</td>
-        <td className={clases.cell}>Имя 1</td>
-        <td className={clases.cell}>Имя 1</td>
-      </tr>
-      <tr className={clases.column}>
-        <th className={clases.title}>Название проекта</th>
-        <td className={clases.cell}>Проект 1</td>
-        <td className={clases.cell}>Проект 2</td>
-        <td className={clases.cell}>Проект 3</td>
-      </tr>
-      <tr className={clases.column}>
-        <th className={clases.title}>Статус</th>
-        <td className={clases.cell}>1</td>
-        <td className={clases.cell}>2</td>
-        <td className={clases.cell}>3</td>
-      </tr>
+      <Column />
+      <Column />
+      <Column />
     </tbody>
   )
 }
