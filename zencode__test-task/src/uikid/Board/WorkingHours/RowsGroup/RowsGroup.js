2bbbea40957b3b999633a8e671b63f1063fe1185
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-03-03 17:54:56 +0300

    Создание функциональной компоненты RowsGroup

---
+++ zencode__test-task/src/uikid/Board/WorkingHours/RowsGroup/RowsGroup.js
@@ -0,0 +1,54 @@
+import React, { Fragment } from 'react'
+import clases from './RowsGroup.module.css'
+let RowsGroup = () => {
+  return (
+    <Fragment>
+      <tr>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+      </tr>
+      <tr>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+      </tr>
+      <tr>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>0</td>
+      </tr>
+    </Fragment>
+  )
+}
+export default RowsGroup
