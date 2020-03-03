7012fde197593f7659782e4b2c6dffb83a893ee2
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-03-03 16:58:41 +0300

    Update Board.js

--- zencode__test-task/src/uikid/Board/Board.js
+++ zencode__test-task/src/uikid/Board/Board.js
@@ -1,30 +1,12 @@
 import React from 'react'
 import clases from './Board.module.css'
+import ColumnsGroup from './ColumnsGroup/ColumnsGroup'
 
 class Board extends React.Component {
   render() {
     return (
       <table className={clases.table}>
-        <tbody className={clases.columnsGroup}>
-          <tr className={clases.column}>
-            <th className={clases.title}>Фамилия Имя</th>
-            <td className={clases.cell}>Имя 1</td>
-            <td className={clases.cell}>Имя 1</td>
-            <td className={clases.cell}>Имя 1</td>
-          </tr>
-          <tr className={clases.column}>
-            <th className={clases.title}>Название проекта</th>
-            <td className={clases.cell}>Проект 1</td>
-            <td className={clases.cell}>Проект 2</td>
-            <td className={clases.cell}>Проект 3</td>
-          </tr>
-          <tr className={clases.column}>
-            <th className={clases.title}>Статус</th>
-            <td className={clases.cell}>1</td>
-            <td className={clases.cell}>2</td>
-            <td className={clases.cell}>3</td>
-          </tr>
-        </tbody>
+        <ColumnsGroup />
         <tfoot className={clases.workingHours}>
           <tr>
             <th colSpan="4" className={clases.month}>
