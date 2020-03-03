5f844d4c83a9254fc797d3f91bb81b33aee12566
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-03-03 16:26:59 +0300

    Стилизация таблицы

--- zencode__test-task/src/App.js
+++ zencode__test-task/src/App.js
@@ -1,94 +1,100 @@
 import React from 'react'
-import './App.css'
+import clases from './App.module.css'
 
 let App = () => {
   return (
-    <table>
-      <thead>
+    <table className={clases.table}>
+      <tbody className={clases.columnsGroup}>
+        <tr className={clases.column}>
+          <th className={clases.title}>Фамилия Имя</th>
+          <td className={clases.cell}>Имя 1</td>
+          <td className={clases.cell}>Имя 1</td>
+          <td className={clases.cell}>Имя 1</td>
+        </tr>
+        <tr className={clases.column}>
+          <th className={clases.title}>Название проекта</th>
+          <td className={clases.cell}>Проект 1</td>
+          <td className={clases.cell}>Проект 2</td>
+          <td className={clases.cell}>Проект 3</td>
+        </tr>
+        <tr className={clases.column}>
+          <th className={clases.title}>Статус</th>
+          <td className={clases.cell}>1</td>
+          <td className={clases.cell}>2</td>
+          <td className={clases.cell}>3</td>
+        </tr>
+      </tbody>
+      <tfoot className={clases.workingHours}>
         <tr>
-          <th>Фамилия Имя</th>
-          <td>Имя 1</td>
-          <td>Имя 1</td>
-          <td>Имя 1</td>
+          <th colSpan="4" className={clases.month}>
+            Февраль
+          </th>
+          <th colSpan="4" className={clases.month}>
+            Март
+          </th>
+          <th colSpan="5" className={clases.month}>
+            Апрель
+          </th>
         </tr>
         <tr>
-          <th>Название проекта</th>
-          <td>Проект 1</td>
-          <td>Проект 2</td>
-          <td>Проект 3</td>
+          <td className={clases.week}>6</td>
+          <td className={clases.week}>7</td>
+          <td className={clases.week}>8</td>
+          <td className={clases.week}>9</td>
+          <td className={clases.week}>10</td>
+          <td className={clases.week}>11</td>
+          <td className={clases.week}>12</td>
+          <td className={clases.week}>13</td>
+          <td className={clases.week}>14</td>
+          <td className={clases.week}>15</td>
+          <td className={clases.week}>16</td>
+          <td className={clases.week}>17</td>
+          <td className={clases.week}>18</td>
         </tr>
         <tr>
-          <th>Статус</th>
-          <td>1</td>
-          <td>2</td>
-          <td>3</td>
-        </tr>
-      </thead>
-      <tfoot>
-        <tr>
-          <th colSpan="4">Февраль</th>
-          <th colSpan="4">Март</th>
-          <th colSpan="5">Апрель</th>
-        </tr>
-        <tr>
-          <td>6</td>
-          <td>7</td>
-          <td>8</td>
-          <td>9</td>
-          <td>10</td>
-          <td>11</td>
-          <td>12</td>
-          <td>13</td>
-          <td>14</td>
-          <td>15</td>
-          <td>16</td>
-          <td>17</td>
-          <td>18</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
         </tr>
         <tr>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
         </tr>
         <tr>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-        </tr>
-        <tr>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
+          <td className={clases.cellhours}>0</td>
         </tr>
       </tfoot>
     </table>
