a8cfc505cd8c1186ad9dd7aaa3e0f9b42919affc
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-03-04 13:40:57 +0300

    Создание функциональной компоненты EditableCell

---
+++ zencode__test-task/src/uikid/Cells/EditableCell/EditableCell.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import clases from './EditableCell.module.css'
+
+let EditableCell = () => {
+  return <input className={clases.editable} placeholder="1" />
+}
+export default EditableCell
