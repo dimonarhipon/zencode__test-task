958a34d45f28ee6f48725a84c9c7a9717b32c0db
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-03-04 18:13:44 +0300

    Update EditableCell.js

--- zencode__test-task/src/uikid/Cells/EditableCell/EditableCell.js
+++ zencode__test-task/src/uikid/Cells/EditableCell/EditableCell.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import clases from './EditableCell.module.css'
 
-let EditableCell = () => {
-  return <input className={clases.editable} placeholder="1" />
+let EditableCell = props => {
+  return (
+    <Fragment>
+      <input
+        className={clases.editable}
+        placeholder={
+          props.titleColumn || props.text || props.title || props.rowsData
+        }
+      />
+    </Fragment>
+  )
 }
 export default EditableCell
