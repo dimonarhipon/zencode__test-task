420f6f8841ca54782a579c4236416ac0a2c238eb
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-03-04 18:12:58 +0300

    Update Week.js

--- zencode__test-task/src/uikid/Board/WorkingHours/MounthsWeeks/Week/Week.js
+++ zencode__test-task/src/uikid/Board/WorkingHours/MounthsWeeks/Week/Week.js
@@ -2,22 +2,12 @@ import React, { Fragment } from 'react'
 // import clases from './Week.module.css'
 import UneditableCell from '../../../../Cells/UneditableCell/UneditableCell'
 
-let Week = () => {
+let Week = props => {
   return (
     <Fragment>
-      <UneditableCell />
-      <UneditableCell />
-      <UneditableCell />
-      <UneditableCell />
-      <UneditableCell />
-      <UneditableCell />
-      <UneditableCell />
-      <UneditableCell />
-      <UneditableCell />
-      <UneditableCell />
-      <UneditableCell />
-      <UneditableCell />
-      <UneditableCell />
+      {props.weeksData.map(({ week }) => (
+        <UneditableCell week={week} />
+      ))}
     </Fragment>
   )
 }
