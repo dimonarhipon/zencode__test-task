f1dba2ebd01111e53e6170a8ec2de58b0572b916
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-03-04 11:16:16 +0300

    Создание функциональной компоненты HeaderColumn

---
+++ zencode__test-task/src/uikid/Board/ColumnsGroup/ColumnHeader/ColumnHeader.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import clases from './ColumnHeader.module.css'
+
+let HeaderColumn = () => {
+  return <th className={clases.title}>Фамилия Имя</th>
+}
+export default HeaderColumn
