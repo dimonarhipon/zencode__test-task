9216da08d73fb62bd6e754c98b2b12abc61ada01
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-03-04 13:44:03 +0300

    Update Column.js

--- zencode__test-task/src/uikid/Board/ColumnsGroup/Column/Column.js
+++ zencode__test-task/src/uikid/Board/ColumnsGroup/Column/Column.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import clases from './Column.module.css'
 import HeaderColumn from '../ColumnHeader/ColumnHeader'
+import EditableCell from '../../../Cells/EditableCell/EditableCell'
 
 let Column = () => {
   return (
     <tr className={clases.column}>
       <HeaderColumn />
-      <td className={clases.cell}>Имя 1</td>
-      <td className={clases.cell}>Имя 1</td>
-      <td className={clases.cell}>Имя 1</td>
+      <td className={clases.cell}>
+        <EditableCell />
+      </td>
+      <td className={clases.cell}>
+        <EditableCell />
+      </td>
+      <td className={clases.cell}>
+        <EditableCell />
+      </td>
     </tr>
   )
 }
