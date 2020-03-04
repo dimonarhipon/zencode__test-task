714d89eabe0d35b6e3b12dd72f6d89f642ab76dc
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-03-04 11:22:27 +0300

    Создание функциональной компоненты Week

---
+++ zencode__test-task/src/uikid/Board/WorkingHours/MounthsWeeks/Week/Week.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react'
+import clases from './Week.module.css'
+
+let Week = () => {
+  return (
+    <Fragment>
+      <td className={clases.week}>6</td>
+      <td className={clases.week}>7</td>
+      <td className={clases.week}>8</td>
+      <td className={clases.week}>9</td>
+      <td className={clases.week}>10</td>
+      <td className={clases.week}>11</td>
+      <td className={clases.week}>12</td>
+      <td className={clases.week}>13</td>
+      <td className={clases.week}>14</td>
+      <td className={clases.week}>15</td>
+      <td className={clases.week}>16</td>
+      <td className={clases.week}>17</td>
+      <td className={clases.week}>18</td>
+    </Fragment>
+  )
+}
+export default Week
