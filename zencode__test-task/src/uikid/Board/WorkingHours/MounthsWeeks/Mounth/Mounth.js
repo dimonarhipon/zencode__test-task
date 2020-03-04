f79e18d568b1186f4f2dc4e0ebabb339e5036908
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-03-04 13:44:49 +0300

    Update Mounth.js

--- zencode__test-task/src/uikid/Board/WorkingHours/MounthsWeeks/Mounth/Mounth.js
+++ zencode__test-task/src/uikid/Board/WorkingHours/MounthsWeeks/Mounth/Mounth.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react'
 import clases from './Mounth.module.css'
+import EditableCell from '../../../../Cells/EditableCell/EditableCell'
 
 let Mounth = () => {
   return (
     <Fragment>
       <th colSpan="4" className={clases.month}>
-        Февраль
+        <EditableCell />
       </th>
     </Fragment>
   )
