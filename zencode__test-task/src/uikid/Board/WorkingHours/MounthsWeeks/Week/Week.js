b0dd351890fec441c64d07d2a7e73f725f0f36c1
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-03-04 11:40:58 +0300

    Update Week.js

--- zencode__test-task/src/uikid/Board/WorkingHours/MounthsWeeks/Week/Week.js
+++ zencode__test-task/src/uikid/Board/WorkingHours/MounthsWeeks/Week/Week.js
@@ -1,22 +1,23 @@
 import React, { Fragment } from 'react'
-import clases from './Week.module.css'
+// import clases from './Week.module.css'
+import UneditableCell from '../../../../Cells/UneditableCell/UneditableCell'
 
 let Week = () => {
   return (
     <Fragment>
-      <td className={clases.week}>6</td>
-      <td className={clases.week}>7</td>
-      <td className={clases.week}>8</td>
-      <td className={clases.week}>9</td>
-      <td className={clases.week}>10</td>
-      <td className={clases.week}>11</td>
-      <td className={clases.week}>12</td>
-      <td className={clases.week}>13</td>
-      <td className={clases.week}>14</td>
-      <td className={clases.week}>15</td>
-      <td className={clases.week}>16</td>
-      <td className={clases.week}>17</td>
-      <td className={clases.week}>18</td>
+      <UneditableCell />
+      <UneditableCell />
+      <UneditableCell />
+      <UneditableCell />
+      <UneditableCell />
+      <UneditableCell />
+      <UneditableCell />
+      <UneditableCell />
+      <UneditableCell />
+      <UneditableCell />
+      <UneditableCell />
+      <UneditableCell />
+      <UneditableCell />
     </Fragment>
   )
 }
