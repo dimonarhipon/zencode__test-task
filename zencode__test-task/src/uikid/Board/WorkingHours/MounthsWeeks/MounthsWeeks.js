0d98cf1af76aa7f89a54b91049cf2c1c298cc66a
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-03-04 11:21:44 +0300

    Update MounthsWeeks.js

--- zencode__test-task/src/uikid/Board/WorkingHours/MounthsWeeks/MounthsWeeks.js
+++ zencode__test-task/src/uikid/Board/WorkingHours/MounthsWeeks/MounthsWeeks.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
-import clases from './MounthsWeeks.module.css'
+// import clases from './MounthsWeeks.module.css'
 import Mounth from './Mounth/Mounth'
+import Week from './Week/Week'
 
 let MounthsWeeks = () => {
   return (
@@ -10,21 +11,8 @@ let MounthsWeeks = () => {
         <Mounth />
         <Mounth />
       </tr>
-
       <tr>
-        <td className={clases.week}>6</td>
-        <td className={clases.week}>7</td>
-        <td className={clases.week}>8</td>
-        <td className={clases.week}>9</td>
-        <td className={clases.week}>10</td>
-        <td className={clases.week}>11</td>
-        <td className={clases.week}>12</td>
-        <td className={clases.week}>13</td>
-        <td className={clases.week}>14</td>
-        <td className={clases.week}>15</td>
-        <td className={clases.week}>16</td>
-        <td className={clases.week}>17</td>
-        <td className={clases.week}>18</td>
+        <Week />
       </tr>
     </Fragment>
   )
