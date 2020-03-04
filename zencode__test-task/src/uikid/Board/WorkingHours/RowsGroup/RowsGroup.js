2563bcc61e9d2ac46b6782aabf23da34325dac88
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-03-04 13:45:01 +0300

    Update RowsGroup.js

--- zencode__test-task/src/uikid/Board/WorkingHours/RowsGroup/RowsGroup.js
+++ zencode__test-task/src/uikid/Board/WorkingHours/RowsGroup/RowsGroup.js
@@ -1,52 +1,131 @@
 import React, { Fragment } from 'react'
 import clases from './RowsGroup.module.css'
+import EditableCell from '../../../Cells/EditableCell/EditableCell'
 let RowsGroup = () => {
   return (
     <Fragment>
       <tr>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
       </tr>
       <tr>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
       </tr>
       <tr>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
-        <td className={clases.cellhours}>0</td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
+        <td className={clases.cellhours}>
+          <EditableCell />
+        </td>
       </tr>
     </Fragment>
   )
