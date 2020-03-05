fef7ed0d0aeaa34efa129d881986b840ec30939d
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-03-05 19:36:42 +0300

    Update Row.js

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -5,44 +5,49 @@ import clases from './Row.module.css'
 let Row = props => {
   return (
     <tr>
+      {/* {props.Row_1.map(({ number }) => (
+        <td className={clases.cellhours}>
+          <Input number={number} />
+        </td>
+      ))} */}
       <td className={clases.cellhours}>
-        <Input />
+        <Input week={props.week1} />
       </td>
       <td className={clases.cellhours}>
-        <Input />
+        <Input week={props.week2} />
       </td>
       <td className={clases.cellhours}>
-        <Input />
+        <Input week={props.week3} />
       </td>
       <td className={clases.cellhours}>
-        <Input />
+        <Input week={props.week4} />
       </td>
       <td className={clases.cellhours}>
-        <Input />
+        <Input week={props.week5} />
       </td>
       <td className={clases.cellhours}>
-        <Input />
+        <Input week={props.week6} />
       </td>
       <td className={clases.cellhours}>
-        <Input />
+        <Input week={props.week7} />
       </td>
       <td className={clases.cellhours}>
-        <Input />
+        <Input week={props.week8} />
       </td>
       <td className={clases.cellhours}>
-        <Input />
+        <Input week={props.week9} />
       </td>
       <td className={clases.cellhours}>
-        <Input />
+        <Input week={props.week10} />
       </td>
       <td className={clases.cellhours}>
-        <Input />
+        <Input week={props.week11} />
       </td>
       <td className={clases.cellhours}>
-        <Input />
+        <Input week={props.week12} />
       </td>
       <td className={clases.cellhours}>
-        <Input />
+        <Input week={props.week13} />
       </td>
     </tr>
   )
