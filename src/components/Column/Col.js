5515e5ed7e652edc497d3a7307824418bef2605a
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-03-10 13:11:04 +0300

    Рефакторинг Table

--- src/components/Column/Col.js
+++ src/components/Column/Col.js
@@ -1,25 +1,12 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import Input from '../Input/Input'
 import clases from './Col.module.css'
 
 let Column = props => {
   return (
-    <Fragment>
-      <tr className={clases.column}>
-        <th className={clases.title}>
-          <Input />
-        </th>
-        <td className={clases.cell}>
-          <Input />
-        </td>
-        <td className={clases.cell}>
-          <Input />
-        </td>
-        <td className={clases.cell}>
-          <Input />
-        </td>
-      </tr>
-    </Fragment>
+    <th rowSpan={props.row} colSpan={props.col} className={clases.title}>
+      <Input text={props.text} />
+    </th>
   )
 }
 export default Column
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,35 +1,44 @@
 import React from 'react'
 import clases from './Table.module.css'
 import Col from '../Column/Col'
-import Input from '../Input/Input'
+// import Input from '../Input/Input'
 import Row from '../Row/Row'
 
 class Table extends React.Component {
   render() {
     return (
       <table className={clases.table}>
-        <tbody className={clases.columnsGroup}>
-          <Col />
-          <Col />
-          <Col />
-        </tbody>
-        <tfoot className={clases.workingHours}>
+        <thead className={clases.columnsGroup}>
           <tr>
-            <th colSpan='4' className={clases.month}>
-              <Input />
-            </th>
-            <th colSpan='4' className={clases.month}>
-              <Input />
-            </th>
-            <th colSpan='5' className={clases.month}>
-              <Input />
-            </th>
+            <Col row={2} text={"Фамилия Имя"}/>
+            <Col row={2}/>
+            <Col row={2}/>
+            <Col col={4}/>
+            <Col col={4}/>
+            <Col col={5}/>
           </tr>
+          <tr>
+            <td className={clases.week}>6</td>
+            <td className={clases.week}>7</td>
+            <td className={clases.week}>8</td>
+            <td className={clases.week}>9</td>
+            <td className={clases.week}>10</td>
+            <td className={clases.week}>11</td>
+            <td className={clases.week}>12</td>
+            <td className={clases.week}>13</td>
+            <td className={clases.week}>14</td>
+            <td className={clases.week}>15</td>
+            <td className={clases.week}>16</td>
+            <td className={clases.week}>17</td>
+            <td className={clases.week}>18</td>
+          </tr>
+        </thead>
+        <tbody className={clases.workingHours}>
           <Row />
           <Row />
           <Row />
           <Row />
-        </tfoot>
+        </tbody>
       </table>
     )
   }
